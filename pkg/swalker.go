d79b0a32d876ee67681ea87744bbca62158b9776
yhuang69 <weapongil@hotmail.com>
2018-03-19 23:06:50 -0700

    first commit

---
+++ pkg/swalker.go
@@ -0,0 +1,97 @@
+package swalker
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+func GetValueOf(data interface{}, key string) reflect.Value {
+	key = sanitizeKey(key)
+	value := flatten(reflect.ValueOf(data))
+	return getValueOf(value, key)
+}
+
+func sanitizeKey(key string) string {
+	buffer := make([]byte, len(key))
+	lhs, rhs := 0, 0
+	for rhs < len(key) {
+		if key[rhs] != '.' || (lhs > 0 && buffer[lhs-1] != '.') {
+			buffer[lhs] = key[rhs]
+			lhs++
+		}
+		rhs++
+	}
+	if lhs > 0 && buffer[lhs-1] == '.' {
+		lhs--
+	}
+	return string(buffer[:lhs])
+}
+
+// Recursion helper
+func getValueOf(value reflect.Value, key string) reflect.Value {
+	if len(key) == 0 {
+		return value
+	}
+
+	var currentValue reflect.Value
+	index := strings.Index(key, ".")
+	if index == -1 {
+		return resolveValue(value, key)
+	}
+
+	current := key[:index]
+	next := key[(index + 1):]
+	currentValue = resolveValue(value, current)
+	return getValueOf(currentValue, next)
+}
+
+func resolveValue(value reflect.Value, key string) reflect.Value {
+	var child reflect.Value
+	if value.Kind() == reflect.Map {
+		child = value.MapIndex(reflect.ValueOf(key))
+	} else {
+		if arrayKey, index, ok := deriveKeyAndIndex(key); ok {
+			child = value.FieldByName(arrayKey).Index(index)
+		} else {
+			child = value.FieldByName(key)
+		}
+	}
+	return flatten(child)
+}
+
+func flatten(value reflect.Value) reflect.Value {
+	// If the child is a pointer type, de-reference to actual value
+	for value.Kind() == reflect.Ptr {
+		value = reflect.Indirect(value)
+	}
+	return value
+}
+
+func deriveKeyAndIndex(key string) (string, int, bool) {
+	left := strings.Index(key, "[")
+	right := strings.LastIndex(key, "]")
+	if left == -1 && right == -1 {
+		return "", -1, false
+	}
+
+	// Panic if the [] are malformed
+	if left == -1 || right == -1 || right < left {
+		panic(fmt.Sprintf(
+			"Malformed key(%s): contains [ or ] or both, but not well formed",
+			key,
+		))
+	}
+
+	realKey := key[:left]
+	index, err := strconv.Atoi(key[(left + 1):right])
+	// Panic if the index is not an integer
+	if err != nil {
+		panic(fmt.Sprintf(
+			"Malformed key(%s): string between [] cannot be intepreted as integer",
+			key,
+		))
+	}
+	return realKey, index, true
+}
---
+++ pkg/swalker_test.go
@@ -0,0 +1,71 @@
+package swalker
+
+import (
+	"fmt"
+	"testing"
+)
+
+type S2 struct {
+	key5 *map[string]string
+	key6 []string
+}
+
+type S3 struct {
+	key8  string
+	key9  []string
+	key10 map[string]string
+}
+
+type SimpleStruct struct {
+	key1  string
+	key2  string
+	key3  string
+	key4  S2
+	key7  map[string]*string
+	key11 *S3
+}
+
+func TestSimpleStruct(t *testing.T) {
+	value6 := map[string]string{
+		"sub-key1": "value1",
+		"sub-key2": "value2",
+	}
+	value7_v1 := "value7_v1"
+	value7 := map[string]*string{
+		"value7_k1": &value7_v1,
+	}
+	value11 := S3{
+		"value8",
+		[]string{"value9_a", "value9_b"},
+		map[string]string{
+			"value10_k1": "value10_v1",
+			"value10_k2": "value10_v2",
+		},
+	}
+	value4 := S2{
+		&value6,
+		[]string{"value6_a", "value6_b"},
+	}
+
+	data := &SimpleStruct{
+		"value1",
+		"value2",
+		"value3",
+		value4,
+		value7,
+		&value11,
+	}
+
+	key := "key4.key5.sub-key1"
+	value := GetValueOf(data, key)
+	fmt.Printf("%v\n", value)
+
+	value = GetValueOf(data, "key4.key6[1]")
+	fmt.Printf("%v\n", value)
+
+	value = GetValueOf(data, "key11.key10.value10_k2")
+	fmt.Printf("%v\n", value)
+
+	value = GetValueOf(data, "key11.key10.")
+	fmt.Printf("%v\n", value)
+}
